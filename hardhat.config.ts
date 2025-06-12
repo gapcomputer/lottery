1529b09aefb5778a0afeb88d3b7796bd5bc6737c
gapcomputer <gapcomputer@users.noreply.github.com>
2025-06-12 02:38:40 +0000

    Simplify Hardhat configuration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,12 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
-  networks: {
-    hardhat: {
-      chainId: 1337
-    }
-  }
+  solidity: "0.8.19"
 };
 
 export default config;
